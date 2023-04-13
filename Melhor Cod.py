93deb93a1f602a797ee2a80ded7f02bbb1e700a6
Marcelo Martins <93449600+2MBG@users.noreply.github.com>
2023-04-13 10:37:42 -0300

    Add files via upload

---
+++ Melhor Cod.py
@@ -0,0 +1,7 @@
+cod = str(input('\n\033[33;1mQual é o melhor COD já criado?\033[m ')).strip().upper()
+
+if cod == 'GHOSTS' or cod == 'CALL OF DUTY: GHOSTS':
+    print('\n\033[32;1mParabéns pelo bom gosto!\033[m\n')
+
+else:
+    print('\n\033[31;1mBom, mas não ao nivel do Ghosts.\033[m\n')
