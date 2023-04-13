c3d9dffc8e0d8f9e047d657c26a35fcf0e46fb96
Marcelo Martins <93449600+2MBG@users.noreply.github.com>
2023-04-12 23:28:42 -0300

    Add files via upload

---
+++ Fração.py
@@ -0,0 +1,9 @@
+# Calculando fração
+num1 = int(input('\nDigite o primeiro número da fração: '))
+num2 = int(input('Digite o segundo número da fração: '))
+num3 = int(input('Digite o número do enunciado: '))
+
+num4 = num1 * num3
+num5 = num4 / num2
+
+print(f'\nO resultado é {num5:.1f}!\n')
